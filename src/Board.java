24be685844dc0705c565eae39e3dfaac892db72e
CDLemmi <112967979+CDLemmi@users.noreply.github.com>
2023-12-11 11:52:39 -0800

    first working version

---
+++ src/Board.java
@@ -0,0 +1,119 @@
+import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import java.awt.*;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.HashMap;
+
+public class Board extends JPanel{
+
+    enum Type {EMPTY, BLACK, RED, AVAILABLE}
+
+    Type[] types = new Type[49];
+
+    HashMap<Type, BufferedImage> textures = new HashMap<Type, BufferedImage>();
+
+    private int selected = 0;
+
+    Board() {
+        for(int i = 0; i < 49; i++) {
+            types[i] = Type.EMPTY;
+        }
+        types[0] = Type.RED;
+        types[6] = Type.BLACK;
+        types[42] = Type.BLACK;
+        types[48] = Type.RED;
+        loadTextures();
+        setupMouseListener();
+    }
+
+    private void handleClick(int i) {
+        switch(types[i]) {
+            case EMPTY -> resetAvailable();
+            case AVAILABLE -> {
+                types[i] = types[selected];
+                types[selected] = Type.EMPTY;
+                resetAvailable();
+            }
+            case BLACK, RED -> {
+                resetAvailable();
+                selected = i;
+                int[] a = new int[] {-7,-1,1,7};
+                for(int j : a) {
+                    int k = j + i;
+                    if(k <= 48 && k >= 0) {
+                        if(!(j==1 && k%7 == 0) && !(j==-1 && k%7 == 6))
+                        if(types[k] == Type.EMPTY) types[k] = Type.AVAILABLE;
+
+                    }
+                }
+
+            }
+        }
+        paintComponent(getGraphics());
+    }
+
+    private void resetAvailable() {
+        for(int i = 0; i < 49; i++) {
+            if(types[i] == Type.AVAILABLE) {
+                types[i] = Type.EMPTY;
+            }
+        }
+    }
+
+    private void setupMouseListener() {
+        addMouseListener(new MouseListener() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                int x = e.getX();
+                int y = e.getY();
+                int w = getWidth() / 7;
+                int h = getHeight() / 7;
+                int i = (x/w) + ((y/h)*7);
+                handleClick(i);
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+
+            }
+            @Override
+            public void mouseReleased(MouseEvent e) {
+
+            }
+            @Override
+            public void mouseEntered(MouseEvent e) {
+
+            }
+            @Override
+            public void mouseExited(MouseEvent e) {
+
+            }
+        });
+    }
+
+    private void loadTextures() {
+        String path = "res";
+        System.out.println(new File("").getAbsolutePath());
+        for(Type type : Type.values()) {
+            try {
+                BufferedImage image = ImageIO.read(new File(path + "\\" + type.toString() + ".png"));
+                textures.put(type, image);
+            } catch(IOException e ) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    protected void paintComponent(Graphics g) {
+        int w = getWidth() / 7;
+        int h = getHeight() / 7;
+        for(int i = 0; i < 49; i++) {
+            g.drawImage(textures.get(types[i]), (i%7)*w, (i/7)*h, w, h, null);
+        }
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+import javax.swing.*;
+import java.awt.*;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        JFrame frame = new JFrame("simple board");
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(701,700);
+        frame.setVisible(true);
+        frame.setLayout(new BorderLayout());
+        Board board= new Board();
+        frame.add(board, BorderLayout.CENTER);
+
+        frame.setSize(700,700);
     }
 }
